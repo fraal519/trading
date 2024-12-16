f6b4da7af32f656de211e6c79a0701a6deb1ceb7
Frank Altmeyer <frank.altmeyer@bmw.de>
2024-12-16 13:44:07 +0100

    new file:   trading_bot.py

---
+++ trading_bot.py
@@ -0,0 +1,33 @@
+import yfinance as yf
+from datetime import datetime, timedelta
+import pandas_ta as ta
+
+ticker = "AMZN"
+asset = yf.Ticker(ticker)
+interval_fast = 10
+interval_slow = 30
+currently_holding = False
+tradelog = []
+
+while True:
+    start_date = (datetime.now()-timedelta(days=2)).strftime('%Y-%m-%d')
+    df = asset.history(start=start_date, interval='1m')
+    del df['Dividends']
+    del df['Stock Splits']
+    del df['Volume']
+    
+    df['SMA_fast'] = ta.sma(df['Close'], interval_fast)
+    df['SMA_slow'] = ta.sma(df['Close'], interval_slow)
+    
+    price = df.iloc[-1]['Close']
+    if df.iloc[-1]['SMA_fast'] > df.iloc[-1]['SMA_slow'] and not currently_holding:
+        print(f"Buy {ticker}@{price}")
+        tradelog.append({'date':datetime.now(),'ticker': ticker,'side': 'buy','price': price})
+        currently_holding = True
+    
+    elif df.iloc[-1]['SMA_fast'] < df.iloc[-1]['SMA_slow'] and currently_holding:
+        print(f"Sell {ticker}@{price}")
+        tradelog.append({'date':datetime.now(),'ticker': ticker,'side': 'sell','price': price})
+        currently_holding = False
+    
+    time.sleep(60)
